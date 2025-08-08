774260a1122202fc483ccb3a5eae1dd04c34ae8f
Iceblad3 <ttanmay1193@gmail.com>
2025-08-07 23:30:31 -0400

    Create password_checker.py

---
+++ password_checker.py
@@ -0,0 +1,20 @@
+# password_checker.py
+import re
+
+def check_password_strength(password):
+    length_error = len(password) < 8
+    digit_error = re.search(r"\d", password) is None
+    uppercase_error = re.search(r"[A-Z]", password) is None
+    lowercase_error = re.search(r"[a-z]", password) is None
+    symbol_error = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~" + r'"]', password) is None
+
+    errors = {
+        "length_error": length_error,
+        "digit_error": digit_error,
+        "uppercase_error": uppercase_error,
+        "lowercase_error": lowercase_error,
+        "symbol_error": symbol_error,
+    }
+
+    strength = 5 - sum(errors.values())  # 0 to 5 scale
+    return strength, errors
